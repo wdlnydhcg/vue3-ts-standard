8fa1f1d6259de29b9546d07bd4386467c4a9ca5a
zhong mingyuan <mralenzhong@qq.com>
2022-01-15 14:37:46 +0800

    [ADD] 增加vscode的配置

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,10 @@
  * @Author: MrAlenZhong
  * @Date: 2022-01-15 13:40:39
  * @LastEditors: MrAlenZhong
- * @LastEditTime: 2022-01-15 14:32:45
+ * @LastEditTime: 2022-01-15 14:34:04
  * @Description:
  */
+
 const { defineConfig } = require("@vue/cli-service");
 // const path = require("path");
 module.exports = defineConfig({
